ae9c547c99cf41df65fc57a6c0954240a3a2daa3
Reza Vilera <vilera_reza99@yahoo.com>
2022-11-17 20:16:10 +0700

    test

--- faceobject/views.py
+++ faceobject/views.py
@@ -8,8 +8,12 @@ from .models import FaceObject
 class FaceList (generics.ListCreateAPIView):
     queryset = FaceObject.objects.all()
     serializer_class = FaceSerializer
-    #def post(self, request, *args, **kwargs):
-        #return self.create(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        exist = FaceObject.objects.filter(faceID = request.data['faceID'])  # exist -> queryset object
+        if exist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return self.create(request, *args, **kwargs)
 
 class FaceDetail (generics.RetrieveUpdateDestroyAPIView):
     queryset = FaceObject.objects.all()
