2e5d19d8ace686da4ea88e84a59472640f4a13ac
Reza Vilera <vilera_reza99@yahoo.com>
2022-11-17 20:37:42 +0700

    update the ai_manager class object when new data is added

--- faceobject/views.py
+++ faceobject/views.py
@@ -3,17 +3,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FaceSerializer
 from .models import FaceObject
+import ai_manager
 
 # Create your views here.
 class FaceList (generics.ListCreateAPIView):
     queryset = FaceObject.objects.all()
     serializer_class = FaceSerializer
+
+    aiManager = ai_manager.aiManager
+
     def post(self, request, *args, **kwargs):
         exist = FaceObject.objects.filter(faceID = request.data['faceID'])  # exist -> queryset object
         if exist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
-            return self.create(request, *args, **kwargs)
+            response = self.create(request, *args, **kwargs)
+            with self.aiManager.condition:
+                self.aiManager.get_class_objects()
+
 
 class FaceDetail (generics.RetrieveUpdateDestroyAPIView):
     queryset = FaceObject.objects.all()
